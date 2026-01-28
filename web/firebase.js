0fe190e52c8b88713895d61c525cc3c7c602218f
mikechaison <minko.vad005@gmail.com>
2026-01-28 08:52:15 +0200

    released web and desktop

--- web/firebase.js
+++ web/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore, collection } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVxCQijUicaV6aOpZcA4rC8k4hcqoSekQ",
-  authDomain: "file-manager-5acb8.firebaseapp.com",
-  projectId: "file-manager-5acb8",
-  storageBucket: "file-manager-5acb8.firebasestorage.app",
-  messagingSenderId: "856728563061",
-  appId: "1:856728563061:web:d1984812507353e522373c",
-  measurementId: "G-NJS0V1FGSN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig)
